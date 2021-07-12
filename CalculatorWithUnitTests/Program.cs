92ecadefe024f99062bf5d82ca965e4afbd557cc
Dmitriy Alhimovich <dmitriy.alhimovich@arcadia.spb.ru>
2021-07-12 11:24:47 +0300

    Add project files.

---
+++ CalculatorWithUnitTests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculatorWithUnitTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
